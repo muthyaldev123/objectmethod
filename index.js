419fd3a22adfb1bb62ea72903a48f80a1d02905b
muthyaldev123 <pulikantimuthyalreddy@gmail.com>
2024-09-02 23:27:03 +0530

    object

---
+++ index.js
@@ -0,0 +1,30 @@
+// obj = { a: 1, b: 2, c: 3, d: 4 };
+// res = Object.fromEntries(Object.entries(obj).filter(([key, value]) => value > 2));
+// console.log(res)
+
+
+// obj1 = { a: 1, b: 2 }
+// obj2 = { c: 3, d: 4 }
+// Object.assign(obj1, obj2)
+// console.log(obj1)
+
+// obj3 = { a: 1, b: 2, c: 3, d: 4 }
+// res = Object.values(obj3).reduce((a, b) => a + b)
+// console.log(res)
+
+// let arr = [
+//   obj1 = { Name: 'Prakash', Age: 21, Gender: 'Male', Country: 'SriLanka' },
+//   obj2 = { Name: 'Ram', Age: 27, Gender: 'Male', },
+//   obj3 = { Name: 'Kiran', Age: 24, Gender: 'Male', Country: 'Australia' },
+//   obj4 = { Name: 'Ravi', Age: 26, Gender: 'Male', },
+//   obj5 = { Name: 'Ram', Age: 27, Gender: 'Male', Country: 'Afghanistan' },
+//   obj6 = { Name: 'Prakash', Age: 27, Gender: 'Male', }
+// ]
+
+// for (i = 0; i < arr.length; i++) {
+//   if (!arr[i].hasOwnProperty('Country')) {
+//       arr[i].Country = "India"
+//   }
+// }
+// console.log(arr)
+  
